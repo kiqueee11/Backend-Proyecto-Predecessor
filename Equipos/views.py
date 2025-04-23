2127b6afe942f0b348267291ba17d6a1ad7bc53a
Your Name <you@example.com>
2025-04-23 13:31:42 +0200

    subo el calculador de winrate

--- Equipos/views.py
+++ Equipos/views.py
@@ -11,8 +11,10 @@ class CreateEquipoView(APIView):
         data = request.data
 
         serializer = serializadorCrearEquipo(data=data)
+        
         if serializer.is_valid():
-            serializer.save()
+            equipo = serializer.save()
+            equipo.calcular_winrate()
             return Response({"succes":"Equipo creado correctamente", "data": serializer.data}, status=201)
         else:
             return Response({"error":serializer.errors}, status=400)
@@ -23,3 +25,22 @@ class GetAllEquiposView(APIView):
         equipos = Equipo.objects.all()
         serializer = serializadorEquipo(equipos, many=True)
         return Response({"succes":"Equipos encontrados","data": serializer.data}, status=200)
+    
+class UpdateEquipoView(APIView):
+    
+    def post(self, request, slug):
+        try:
+            equipo = Equipo.objects.get(slug=slug)
+        except Equipo.DoesNotExist:
+            return Response({"error":"Equipo no encontrado"}, status=404)
+
+        data = request.data
+
+        serializer = serializadorEquipo(instance=equipo, data=data, partial=True)
+        
+        if serializer.is_valid():
+            equipo = serializer.save()
+            equipo.calcular_winrate()
+            return Response({"succes":"Equipo actualizado correctamente", "data": serializer.data}, status=201)
+        else:
+            return Response({"error":serializer.errors}, status=400)
